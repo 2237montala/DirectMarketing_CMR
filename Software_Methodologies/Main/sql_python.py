f54fd7cd78b49b889b8b4793fd8bb45c20c325f9
ulycar11 <46250723+ulycar11@users.noreply.github.com>
2019-02-10 12:42:55 -0600

    Added Python Project Folder
    You can open this folder from your IDE if you set up correctly. For me it is under "/users/Ulysses/Documents/Github/DirectMarketing_CMR/Software_Methodologies"

---
+++ Software_Methodologies/Main/sql_python.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+sqlite_file = 'my_first_db.sqlite'
+table_name1 = 'my_table_1'
+table_name2 = 'my_table_2'
+new_field = 'my_1st_column'
+field_type =  'INTEGER'
+
+conn = sqlite3.connect()
+c = conn.cursor()
+
+c.execute('CREATE TABLE {tn} ({nf} {ft})'\
+          .format(tn = table_name1, nf = new_field, ft = field_type))
+
+c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
+        .format(tn=table_name2, nf=new_field, ft=field_type))
+
+conn.commit()
+conn.close()
