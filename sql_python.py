fd9d362b9bd357f1419a1ca45321a3ad2905575e
ulycar11 <46250723+ulycar11@users.noreply.github.com>
2019-02-18 13:33:30 -0600

    Created new sqlite file

--- sql_python.py
+++ sql_python.py
@@ -1,20 +1,25 @@
 #https://sebastianraschka.com/Articles/2014_sqlite_in_python_tutorial.html
 import sqlite3
 
-sqlite_file = 'my_first_db.sqlite'
-table_name1 = 'my_table_1'
-table_name2 = 'my_table_2'
-new_field = 'my_1st_column'
-field_type =  'INTEGER'
+sqlite_file = 'my_first_db.sqlite'    # name of the sqlite database file
+table_name1 = 'my_table_1'  # name of the table to be created
+table_name2 = 'my_table_2'  # name of the table to be created
+new_field = 'my_1st_column' # name of the column
+field_type = 'INTEGER'  # column data type
 
-conn = sqlite3.connect()
+# Connecting to the database file
+conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
+# Creating a new SQLite table with 1 column
 c.execute('CREATE TABLE {tn} ({nf} {ft})'\
-          .format(tn = table_name1, nf = new_field, ft = field_type))
+        .format(tn=table_name1, nf=new_field, ft=field_type))
 
+# Creating a second table with 1 column and set it as PRIMARY KEY
+# note that PRIMARY KEY column must consist of unique values!
 c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
         .format(tn=table_name2, nf=new_field, ft=field_type))
 
+# Committing changes and closing the connection to the database file
 conn.commit()
 conn.close()
