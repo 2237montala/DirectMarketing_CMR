a9cb40fb4cdabe56957cea76fa7ea3746ddb4a32
ulycar11 <46250723+ulycar11@users.noreply.github.com>
2019-02-22 04:08:55 -0600

    Simple SQLite methods created

--- sql_python.py
+++ sql_python.py
@@ -1,34 +1,49 @@
-#https://sebastianraschka.com/Articles/2014_sqlite_in_python_tutorial.html
-#https://www.dataquest.io/blog/python-pandas-databases/
+# https://sebastianraschka.com/Articles/2014_sqlite_in_python_tutorial.html
+# https://www.dataquest.io/blog/python-pandas-databases/
+# https://www.youtube.com/watch?v=pd-0G0MigUA
 import sqlite3
 from Ingestor import Ingestor
 
-sqlite_file = 'my_first_db.sqlite'
+# SETTING IT EQUAL TO ':memory:' WILL HAVE IT RUN ON RAM AND NO SQLITE FILE WILL BE MADE.
+sqlite_file = 'test.db'
 
 # Connecting to the database file
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
-def create_columns(data, tablename):
-    Ingestor.getheader()
-    cur.execute("create table %s (% integer, departure date, arrival date, number text, route_id integer)"% tablename)    
-    conn.commit()
+def create_table(table_name, column_name, column_type):
+    with conn:
+        c.execute("CREATE TABLE %s (%s %s PRIMARY KEY)" % (table_name, column_name, column_type))
+
+
+def return_table(tablename):
+    c.execute("SELECT * FROM %s" % tablename)
+    return c.fetchall()
     
+def add_column(tablename,column_name, column_type): 
+    c.execute("ALTER TABLE %s ADD COLUMN %s %s" % (tablename, column_name, column_type))
+# STILL NEED TO FIGURE OUT HOW TO HAVE INGESTOR CLASS AUTOMATICALLY CREATE CORRECT AMOUNT OF COLUMNS
+
+# AFTER THAT IS DONE, ADDING ROW COMMAND HAS TO CHNAGE DEPENDING ON HOW MANY COLUMNS TEHRE IS. THINKING OF PROBABLY DOING A FOR LOOP.
+def add_row(tablename, column, row): 
+    with conn:
+        if column == 'Name':
+            c.execute("INSERT OR IGNORE INTO %s (%s) VALUES (?)" % (tablename, column), (row,) )
+        elif column == 'Number':
+            c.execute("INSERT OR IGNORE INTO %s (%s) VALUES (%d)" % (tablename, column, row))
+
 # Creating a new SQLite table with 1 column
-#cur.execute("create table daily_flights (id integer, departure date, arrival date, number text, route_id integer)")
-
-<<<<<<< HEAD
-# Creating a second table with 1 column and set it as PRIMARY KEY
-# note that PRIMARY KEY column must consist of unique values!
-c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
-        .format(tn=table_name2, nf=new_field, ft=field_type))
-
-# Committing changes and closing the connection to the database file
-conn.commit()
-conn.close()
-=======
-#c.execute('CREATE TABLE {tn} ({nf} {ft})'\
-#       .format(tn=table_name1, nf=new_field, ft=field_type))
-#conn.commit()
-conn.close()
->>>>>>> d78b097d82dbdb559382e312bd6f2557af0fe47e
+t = 'TEST_TABLE'
+create_table(t, 'Name', 'string')
+add_column(t,'Number', 'string')
+add_column(t,'Equity', 'integer')
+add_row(t, 'Name', 'John')
+add_row(t, 'Number', 1)
+add_row(t, 'Number', 2)
+add_row(t, 'Number', 3)
+# c.execute("DELETE FROM t")
+# conn.commit()
+# c.execute("DROP TABLE %s" % t)
+# conn.commit()
+# print(return_table(t))
+# conn.close()
