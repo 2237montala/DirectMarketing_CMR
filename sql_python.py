ed456d4f357c54e01ac5b27bb27ae549a03cb037
2237montala <anthony.montalbano@marquette.edu>
2019-02-22 13:11:48 -0600

    Added anthony's changes

--- sql_python.py
+++ sql_python.py
@@ -16,44 +16,65 @@ def create_table(table_name, column_name, column_type):
     with conn:
         if  not doesTableExist(table_name):
             c.execute("CREATE TABLE %s (%s %s PRIMARY KEY)" % (table_name, column_name, column_type))
+            return True
+        else:
+            #Returns false if tabel already exists
+            return False
 
-def doesTableExist(table_name,file_name):
-    c.execute("""SELECT COUNT(*)FROM %s WHERE table_name = '{0}'""".format(table_name.replace('\'', '\'\''))% file_name)
-
-    if c.fetchone()[0] == 1:
-        return True
-    else:
+def doesTableExist(table_name):
+    try:
+        c.execute("""SELECT COUNT(*) FROM %s WHERE 'TEST_TABLE' """ % (table_name))
+        #c.execute("""SELECT COUNT(*)FROM %s WHERE table_name = '{0}'""".format(table_name.replace('\'', '\'\''))% table_name)
+        #c.execute("""SELECT COUNT(*)FROM information_schema.tables WHERE table_name = '{0}'""".format(table_name.replace('\'', '\'\'')))
+        if c.fetchone()[0] == 0:
+            print("Table exsists")
+            return True
+        else:
+            print("Table doesn't exist")
+            return False
+    except:
+        print("Table doesn't exist")
         return False
 
 
 def return_table(tablename):
-    c.execute("SELECT * FROM %s" % tablename)
-    return c.fetchall()
+    try:
+        c.execute("SELECT * FROM %s" % tablename)
+        return c.fetchall()
+    except:
+        return "Table doesn't exist"
 
 def add_column(tablename,column_name, column_type):
-    c.execute("ALTER TABLE %s ADD COLUMN %s %s" % (tablename, column_name, column_type))
-# STILL NEED TO FIGURE OUT HOW TO HAVE INGESTOR CLASS AUTOMATICALLY CREATE CORRECT AMOUNT OF COLUMNS
+    try:
+        c.execute("ALTER TABLE %s ADD COLUMN %s %s" % (tablename, column_name, column_type))
+        return True
+    except:
+        return False
+        # STILL NEED TO FIGURE OUT HOW TO HAVE INGESTOR CLASS AUTOMATICALLY CREATE CORRECT AMOUNT OF COLUMNS
 
 # AFTER THAT IS DONE, ADDING ROW COMMAND HAS TO CHNAGE DEPENDING ON HOW MANY COLUMNS TEHRE IS. THINKING OF PROBABLY DOING A FOR LOOP.
-def add_row(tablename, column, row):
+def add_row(tablename, column, row, dataType):
     with conn:
-        if column == 'Name':
-            c.execute("INSERT OR IGNORE INTO %s (%s) VALUES (?)" % (tablename, column), (row,) )
-        elif column == 'Number':
-            c.execute("INSERT OR IGNORE INTO %s (%s) VALUES (%d)" % (tablename, column, row))
+        if dataType == 's':
+            c.execute("INSERT OR IGNORE INTO %s (%s) VALUES (%s)" % (tablename, column, row))
+        #if column == 'Name':
+        #    c.execute("INSERT OR IGNORE INTO %s (%s) VALUES (?)" % (tablename, column), (row,) )
+        #elif column == 'Number':
+        #    c.execute("INSERT OR IGNORE INTO %s (%s) VALUES (%d)" % (tablename, column, row))
 
 # Creating a new SQLite table with 1 column
 t = 'TEST_TABLE'
 create_table(t, 'Name', 'string')
-add_column(t,'Number', 'string')
-add_column(t,'Equity', 'integer')
-add_row(t, 'Name', 'John')
-add_row(t, 'Number', 1)
-add_row(t, 'Number', 2)
-add_row(t, 'Number', 3)
-# c.execute("DELETE FROM t")
-# conn.commit()
-# c.execute("DROP TABLE %s" % t)
-# conn.commit()
-# print(return_table(t))
-# conn.close()
+#add_column(t,'Number', 'string')
+#add_column(t,'Equity', 'integer')
+#add_row(t,'Name','John','s')
+#add_row(t, 'Name', 'John')
+# add_row(t, 'Number', 1)
+# add_row(t, 'Number', 2)
+# add_row(t, 'Number', 3)
+#c.execute("DELETE FROM %s" % t)
+#conn.commit()
+#c.execute("DROP TABLE %s" % t)
+#conn.commit()
+print(return_table(t))
+conn.close()
