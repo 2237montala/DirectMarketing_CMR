a46c9fcd06769c1f5e9cdbedcf6227b03894f07a
2237montala <anthony.montalbano@marquette.edu>
2019-02-23 19:33:19 -0600

    Got ingestor to work with database

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -4,7 +4,7 @@ class DatabaseManager:
     def __init__ (self, file_loc):
         self.conn = sqlite3.connect(file_loc)
         self.cursor = self.conn.cursor()
-    
+
     def create_table(self, table_name, column_name, column_type):
         with self.conn:
           #  if  not doesTableExist(table_name):
@@ -15,7 +15,7 @@ class DatabaseManager:
           else:
                 #Returns false if table already exists
                 return False
-    
+
     def doesTableExist(self, table_name):
 #         c.execute("""SELECT COUNT(*) FROM %s WHERE 'TEST_TABLE_1' """ % (table_name))
         try:
@@ -37,34 +37,23 @@ class DatabaseManager:
             return self.cursor.fetchall()
         except:
             return "Table doesn't exist"
-        
+
     def add_column(self, table_name,column_name, column_type):
         try:
             self.cursor.execute("ALTER TABLE %s ADD COLUMN %s %s" % (table_name, column_name, column_type))
             return True
         except:
             return False
-        
+
     def add_row(self, table_name, column, row):
         with self.conn:
                 self.cursor.execute("INSERT OR IGNORE INTO %s (%s) VALUES (?)" % (table_name, column), (row, ))
-            #if column == 'Name':
-            #    c.execute("INSERT OR IGNORE INTO %s (%s) VALUES (?)" % (table_name, column), (row,) )
-            #elif column == 'Number':
-            #    c.execute("INSERT OR IGNORE INTO %s (%s) VALUES (%d)" % (table_name, column, row))
+
     def add_row(self, table_name, column_arr, row_arr):
         with self.conn:
-            self.cursor.execute("INSERT OR IGNORE INTO %s (%s) VALUES (?)" % (table_name, column_arr[0]), (row_arr[0], ))
+            self.cursor.execute("INSERT OR IGNORE INTO %s (%s) VALUES(?)" % (table_name, column_arr[0]), (row_arr[0], ))
             for i in range(1,len(column_arr)):
                 self.cursor.execute("UPDATE %s SET %s='%s' WHERE %s='%s' " % (table_name, column_arr[i], row_arr[i], column_arr[0], row_arr[0]))
-                self.cursor.execute("UPDATE %s SET %s='%s' WHERE %s='%s' " % (table_name, column_arr[i], row_arr[i], column_arr[0], row_arr[0]))
-#                 cur.execute("update airlines set country=? where id=?", values)
-            #if column == 'Name':
-            #    c.execute("INSERT OR IGNORE INTO %s (%s) VALUES (?)" % (table_name, column), (row,) )
-            #elif column == 'Number':
-            #    c.execute("INSERT OR IGNORE INTO %s (%s) VALUES (%d)" % (table_name, column, row))
     def clear_table(self, table_name):
         with self.conn:
                 self.cursor.execute("DELETE FROM %s" % table_name)
-            
-            
--- sql_python.py
+++ sql_python.py
@@ -4,18 +4,37 @@
 #https://stackoverflow.com/questions/17044259/python-how-to-check-if-table-exists
 from DatabaseManager import DatabaseManager
 from Ingestor import Ingestor
-from _hashlib import new
 
 # SETTING IT EQUAL TO ':memory:' WILL HAVE IT RUN ON RAM AND NO SQLITE FILE WILL BE MADE.
 sqlite_file = 'test.db'
 db = DatabaseManager(sqlite_file)
-new_table = 'Students'
-db.create_table(new_table, 'Name', 'string')
-db.add_column(new_table, 'Age', 'integer')
-db.add_column(new_table, 'Year', 'string')
-db.add_row(new_table, ['Name', 'Age','Year'], ['Anthony', '12', '2021'])
-db.add_row(new_table, ['Name', 'Age','Year'], ['Mike', '18', '2022'])
-db.add_row(new_table, ['Name', 'Age','Year'], ['Tyler', '45', '2045'])
-# db.clear_table(new_table)
+
+filename = 
+ingestor = Ingestor(filename)
+ingestor.readCSV()
+
+tempHeaders = ingestor.getCSVHeaders()
+searchCritera = ["1st Owner's First Name","1st Owner's Last Name"]
+
+dict = ingestor.getHeaderIndex(ingestor.listToDict(searchCritera,-1),tempHeaders)
+print("\nDictionary of search critera and their indexes in the csv")
+print(dict)
+
+ingestor.searchRows(dict,ingestor.getRows())
+print("\nPrint filtered list from unfiltered row")
+print(ingestor.getRowAt(1))
+
+new_table = 'Divorse'
+headers = ["First_Name","Last_Name"]
+db.create_table(new_table, headers[0],'string')
+db.add_column(new_table, headers[1], 'string')
+#db.add_row(new_table, "First_Name", "Steve")
+#for person in ingestor.getRows():
+#    db.add_row(new_table, headers, person)
+#db.add_row(new_table, headers, ["Steve","Smith"])
+
 print(db.return_table(new_table))
-# Connecting to the database file
+
+print(db.print_table(new_table))
+
+#db.clear_table(new_table)
