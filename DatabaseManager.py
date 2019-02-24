8d4cdbc939473012a7afb6aa37fad2c1ae7e14f5
2237montala <anthony.montalbano@marquette.edu>
2019-02-24 17:38:55 -0600

    I broke everything

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -20,8 +20,6 @@ class DatabaseManager:
 #         c.execute("""SELECT COUNT(*) FROM %s WHERE 'TEST_TABLE_1' """ % (table_name))
         try:
             self.cursor.execute("SELECT * FROM sqlite_master WHERE name = '%s' AND type = 'table'" % table_name)
-                #c.execute("""SELECT COUNT(*)FROM %s WHERE table_name = '{0}'""".format(table_name.replace('\'', '\'\''))% table_name)
-                #c.execute("""SELECT COUNT(*)FROM information_schema.tables WHERE table_name = '{0}'""".format(table_name.replace('\'', '\'\'')))
             if self.cursor.fetchone()[1] == table_name:
 #                 print("Table exists")
                 return True
@@ -39,10 +37,12 @@ class DatabaseManager:
             return "Table doesn't exist"
 
     def add_column(self, table_name,column_name, column_type):
+        self.cursor.execute("ALTER TABLE %s ADD COLUMN %s %s" % (table_name,'"{}"'.format(column_name) ,column_type))
         try:
-            self.cursor.execute("ALTER TABLE %s ADD COLUMN %s %s" % (table_name, column_name, column_type))
-            return True
+            self.cursor.execute("ALTER TABLE %s ADD COLUMN %s %s" % (table_name,'"{}"'.format(column_name) ,column_type))
+            print('True')
         except:
+            print('False')
             return False
 
     def add_row(self, table_name, column, row):
@@ -51,9 +51,10 @@ class DatabaseManager:
 
     def add_row(self, table_name, column_arr, row_arr):
         with self.conn:
-            self.cursor.execute("INSERT OR IGNORE INTO %s (%s) VALUES(?)" % (table_name, column_arr[0]), (row_arr[0], ))
+            self.cursor.execute("INSERT OR IGNORE INTO %s (%s) VALUES(?)" % (table_name,'"{}"'.format(column_arr[0])), ('"{}"'.format(row_arr[0])))
             for i in range(1,len(column_arr)):
                 self.cursor.execute("UPDATE %s SET %s='%s' WHERE %s='%s' " % (table_name, column_arr[i], row_arr[i], column_arr[0], row_arr[0]))
+
     def clear_table(self, table_name):
         with self.conn:
                 self.cursor.execute("DELETE FROM %s" % table_name)
--- sql_python.py
+++ sql_python.py
@@ -1,5 +1,5 @@
 # https://sebastianraschka.com/Articles/2014_sqlite_in_python_tutorial.html
-# https://www.dataquest.io/blog/python-pandas-databases/
+    # https://www.dataquest.io/blog/python-pandas-databases/
 # https://www.youtube.com/watch?v=pd-0G0MigUA
 #https://stackoverflow.com/questions/17044259/python-how-to-check-if-table-exists
 from DatabaseManager import DatabaseManager
@@ -9,32 +9,48 @@ from Ingestor import Ingestor
 sqlite_file = 'test.db'
 db = DatabaseManager(sqlite_file)
 
-filename = 
+filename = '/home/anthonym/Documents/SchoolWork/SoftwareEngineering/The_lists/Probate 08.19.18.csv'
 ingestor = Ingestor(filename)
 ingestor.readCSV()
 
 tempHeaders = ingestor.getCSVHeaders()
-searchCritera = ["1st Owner's First Name","1st Owner's Last Name"]
+searchCritera = [tempHeaders[2],tempHeaders[3],tempHeaders[5],tempHeaders[15],tempHeaders[16]]
 
-dict = ingestor.getHeaderIndex(ingestor.listToDict(searchCritera,-1),tempHeaders)
+#searchCritera = [tempHeaders[3],tempHeaders[5],tempHeaders[15],tempHeaders[16]]
+
+searchCriteraTwoD = ingestor.getHeaderIndex(searchCritera,tempHeaders)
 print("\nDictionary of search critera and their indexes in the csv")
-print(dict)
+print(searchCriteraTwoD)
 
-ingestor.searchRows(dict,ingestor.getRows())
+ingestor.searchRows(searchCriteraTwoD,ingestor.getRows())
 print("\nPrint filtered list from unfiltered row")
-print(ingestor.getRowAt(1))
+print(ingestor.getRowAt(0))
 
-new_table = 'Divorse'
-headers = ["First_Name","Last_Name"]
-db.create_table(new_table, headers[0],'string')
-db.add_column(new_table, headers[1], 'string')
-#db.add_row(new_table, "First_Name", "Steve")
+for i in range(0,len(searchCriteraTwoD)):
+    pass
+    #searchCriteraTwoD[i][0] = searchCriteraTwoD[i][0].replace(' ','_')
+    #searchCriteraTwoD[i][0] = searchCriteraTwoD[i][0].replace("'",'"')
+
+print(searchCriteraTwoD)
+
+new_table = 'Probate'
+db.create_table(new_table, searchCritera,'string')
+
+for i in range(1,len(searchCritera)):
+    db.add_column(new_table,searchCritera,'string')
+
+#db.add_column(new_table, headers[1], 'string')
+#db.add_row(new_table, ['Site_Address'], ["Steve"])
+#print(ingestor.getRows())
+db.add_row(new_table,searchCritera,ingestor.getRowAt(0))
+
 #for person in ingestor.getRows():
-#    db.add_row(new_table, headers, person)
+    #print(person)
+    #db.add_row(new_table, searchCritera, person)
 #db.add_row(new_table, headers, ["Steve","Smith"])
 
 print(db.return_table(new_table))
 
-print(db.print_table(new_table))
+#print(db.print_table(new_table))
 
-#db.clear_table(new_table)
+db.clear_table(new_table)
