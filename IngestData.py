96d34ae971b7f334e03d84b947126cb497544365
2237montala <anthony.montalbano@marquette.edu>
2019-03-08 18:26:13 -0600

    formating

--- IngestData.py
+++ IngestData.py
@@ -2,23 +2,8 @@
 #file and import it into the database
 #https://www.datacamp.com/community/tutorials/python-excel-tutorial
 #http://zetcode.com/articles/openpyxl/
-#import openpyxl ## library for excel sheet
-
-#FilePath = "/home/anthonym/Documents/SchoolWork/SoftwareEngineering/CMR_TestData.xlsx"
-
-#Open the work book at the file path
-#workBook = openpyxl.load_workbook(filename = FilePath)
-#Open the fist sheet
-#currSheet = workBook.active
-#currSheetDims = currSheet.dimensions
-
-#print(currSheet['A1'].value)
-
-#for row in range(0,currSheet.max_row):
-#    print(row.value)
-
-#print(currSheetDims)
 #https://www.geeksforgeeks.org/working-csv-files-python/
+
 from Ingestor import Ingestor
 
 def main():
