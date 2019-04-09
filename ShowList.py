69ed7fc6e8319b5d39f4c31e100c7f588aa34f9e
2237montala <anthony.montalbano@marquette.edu>
2019-04-08 23:07:32 -0500

    Added new actions in edit menu and filter the search bar input

--- ShowList.py
+++ ShowList.py
@@ -24,67 +24,35 @@ class Ui_MainWindow(QtWidgets.QWidget):
     def __init__(self,db_file):
         super().__init__()
         self.db = DatabaseManager(db_file)
-        self.table_in_db = self.db.get_table_names()
+        self.tables_in_db = self.db.get_table_names()
         self.curr_table = ''
-        if(len(self.table_in_db) > 0):
-            self.curr_table = self.table_in_db[0]
+        if(len(self.tables_in_db) > 0):
+            self.curr_table = self.tables_in_db[0]
 
 
     def setup_main_widget(self,width,height):
+        """
+        Sets up the widgets for the window
+        """
         #self.centralwidget = QtWidgets.QWidget()
         #self.centralwidget.setObjectName("centralwidget")
         #self = QtWidgets.QWidget()
         self.setGeometry(QtCore.QRect(0, 0, width, height))
         self.setObjectName("gridLayoutWidget")
         self.gridLayout = QtWidgets.QGridLayout(self)
-        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setContentsMargins(5, 5, 5, 5)
         self.gridLayout.setSpacing(5)
         self.gridLayout.setObjectName("gridLayout")
-<<<<<<< HEAD
-        self.checkBox_5 = QtWidgets.QCheckBox(self)
-        self.checkBox_5.setObjectName("checkBox_5")
-        self.gridLayout.addWidget(self.checkBox_5, 2, 2, 1, 1)
-        self.checkBox = QtWidgets.QCheckBox(self)
-        self.checkBox.setObjectName("checkBox")
-        self.gridLayout.addWidget(self.checkBox, 1, 1, 1, 1)
-        self.checkBox_4 = QtWidgets.QCheckBox(self)
-        self.checkBox_4.setObjectName("checkBox_4")
-        self.gridLayout.addWidget(self.checkBox_4, 0, 1, 1, 1)
-        self.checkBox_6 = QtWidgets.QCheckBox(self)
-        self.checkBox_6.setObjectName("checkBox_6")
-        self.gridLayout.addWidget(self.checkBox_6, 2, 1, 1, 1)
-        self.checkBox_3 = QtWidgets.QCheckBox(self)
-        self.checkBox_3.setObjectName("checkBox_3")
-        self.gridLayout.addWidget(self.checkBox_3, 1, 2, 1, 1)
-        self.checkBox_2 = QtWidgets.QCheckBox(self)
-        self.checkBox_2.setObjectName("checkBox_2")
-        self.gridLayout.addWidget(self.checkBox_2, 0, 2, 1, 1)
-        self.scrollArea = QtWidgets.QScrollArea(self)
-=======
-        self.searchBar = QtWidgets.QLineEdit(self.gridLayoutWidget)
-        self.searchBar.setObjectName("Search:")
+
+        #Search bar text entry
+        self.searchBar = QtWidgets.QLineEdit(self)
+        self.searchBar.setPlaceholderText("Search Term")
+        self.searchBar.returnPressed.connect(self.get_search_key)
+        self.searchBar.setObjectName("SearchBar")
         self.gridLayout.addWidget(self.searchBar, 1, 2 ,1, 1)
         self.searchBar.setMaximumWidth(500)
-#         self.checkBox_5 = QtWidgets.QCheckBox(self.gridLayoutWidget)
-#         self.checkBox_5.setObjectName("checkBox_5")
-#         self.gridLayout.addWidget(self.checkBox_5, 2, 2, 1, 1)
-#         self.checkBox = QtWidgets.QCheckBox(self.gridLayoutWidget)
-#         self.checkBox.setObjectName("checkBox")
-#         self.gridLayout.addWidget(self.checkBox, 1, 1, 1, 1)
-#         self.checkBox_4 = QtWidgets.QCheckBox(self.gridLayoutWidget)
-#         self.checkBox_4.setObjectName("checkBox_4")
-#         self.gridLayout.addWidget(self.checkBox_4, 0, 1, 1, 1)
-#         self.checkBox_6 = QtWidgets.QCheckBox(self.gridLayoutWidget)
-#         self.checkBox_6.setObjectName("checkBox_6")
-#         self.gridLayout.addWidget(self.checkBox_6, 2, 1, 1, 1)
-#         self.checkBox_3 = QtWidgets.QCheckBox(self.gridLayoutWidget)
-#         self.checkBox_3.setObjectName("checkBox_3")
-#         self.gridLayout.addWidget(self.checkBox_3, 1, 2, 1, 1)
-#         self.checkBox_2 = QtWidgets.QCheckBox(self.gridLayoutWidget)
-#         self.checkBox_2.setObjectName("checkBox_2")
-#         self.gridLayout.addWidget(self.checkBox_2, 0, 2, 1, 1)
-        self.scrollArea = QtWidgets.QScrollArea(self.gridLayoutWidget)
->>>>>>> 8914bf8502fc8df5cfe98677ed0ce0fc0ba2a092
+
+        self.scrollArea = QtWidgets.QScrollArea(self)
         #self.scrollArea.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
         self.scrollArea.setWidgetResizable(True)
         self.scrollArea.setObjectName("scrollArea")
@@ -92,72 +60,45 @@ class Ui_MainWindow(QtWidgets.QWidget):
         #Scroll Window
         self.table = QtWidgets.QTableWidget(20,10)
         self.table.cellDoubleClicked.connect(self.table_item_clicked)
-        #self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 977, 602))
         self.scrollArea.setWidget(self.table)
         self.gridLayout.addWidget(self.scrollArea, 3, 0, 1, 4)
 
-        self.pushButton = QtWidgets.QPushButton(self)
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton.clicked.connect(self.get_search_key)
-        self.gridLayout.addWidget(self.pushButton, 1, 3, 1, 1)
-        self.comboBox = QtWidgets.QComboBox(self)
+        self.searchButton = QtWidgets.QPushButton("Search Table", self)
+        self.searchButton.setObjectName("searchButton")
+        self.searchButton.clicked.connect(self.get_search_key)
+        self.gridLayout.addWidget(self.searchButton, 1, 3, 1, 1)
 
         #Create combo box with the ability to switch tables
+        self.comboBox = QtWidgets.QComboBox(self)
         self.comboBox.setObjectName("comboBox")
         self.add_combo_box_items(self.db.get_table_names(),self.comboBox)
         self.comboBox.activated.connect(self.switch_curr_table_comboBox)
         self.gridLayout.addWidget(self.comboBox, 1, 0, 1, 1)
-
-
-        #
-        self.retranslateUi()
-        # QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(self)
 
     def setup_menu_bar(self):
+        """
+        Sets up the menu bar. Main windows have their own menu bar and this
+        provides a menu bar on completion
+        """
         #Menu Bar
         self.menubar = QtWidgets.QMenuBar()
         self.menubar.setDefaultUp(False)
         self.menubar.setNativeMenuBar(True)
 
         fileMenu = self.menubar.addMenu("File")
+        fileMenu.addMenu("Log out")
         editMenu = self.menubar.addMenu("Edit")
         editMenu.addAction('Import CSV',self.open_csv_import)
+        editMenu.addSeparator()
+        editMenu.addAction('Clear Table',self.clear_table)
+        editMenu.addAction('Delete Table',self.delete_table)
         self.viewMenu = self.menubar.addMenu("View")
         self.add_menu_items(self.db.get_table_names()
                             ,self.viewMenu.addMenu("Switch Table"))
         self.viewMenu.addAction("Update Table", self.update_menu_action)
-        searchMenu = self.menubar.addMenu("Search")
 
         return self.menubar
-
-<<<<<<< HEAD
-    def retranslateUi(self):
-        _translate = QtCore.QCoreApplication.translate
-        #MainWindow.setWindowTitle(_translate("MainWindow", "Direct Marketing CMR"))
-        self.checkBox_5.setText(_translate("MainWindow", "CheckBox"))
-        self.checkBox.setText(_translate("MainWindow", "CheckBox"))
-        self.checkBox_4.setText(_translate("MainWindow", "CheckBox"))
-        self.checkBox_6.setText(_translate("MainWindow", "CheckBox"))
-        self.checkBox_3.setText(_translate("MainWindow", "CheckBox"))
-        self.checkBox_2.setText(_translate("MainWindow", "CheckBox"))
-=======
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-    
-    #Names all the objects in our QWidget Window
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Direct Marketing CMR"))
-        self.searchBar.setText(_translate("MainWindow", "Search Entry"))
-#         self.checkBox_5.setText(_translate("MainWindow", "CheckBox"))
-#         self.checkBox.setText(_translate("MainWindow", "CheckBox"))
-#         self.checkBox_4.setText(_translate("MainWindow", "CheckBox"))
-#         self.checkBox_6.setText(_translate("MainWindow", "CheckBox"))
-#         self.checkBox_3.setText(_translate("MainWindow", "CheckBox"))
-#         self.checkBox_2.setText(_translate("MainWindow", "CheckBox"))
->>>>>>> 8914bf8502fc8df5cfe98677ed0ce0fc0ba2a092
-        self.pushButton.setText(_translate("MainWindow", "Search"))
-
 
     def update_table(self, row_list,header_list):
         """
@@ -180,7 +121,6 @@ class Ui_MainWindow(QtWidgets.QWidget):
         table.horizontalHeader().setSectionResizeMode(1)
         table.setSelectionBehavior(QtWidgets.QTableView.SelectRows);
         self.set_table(table)
-#         print(self.get_table().rowCount())
 
     def add_items(self,table,row_list):
         """
@@ -232,13 +172,17 @@ class Ui_MainWindow(QtWidgets.QWidget):
 
 
     def open_csv_import(self):
+        """
+        Opens the file browser and the csv importer window. On completion
+        the table is updated to the imported table
+        """
         #https://stackoverflow.com/questions/4838890/python-pyqt-popup-window
         #Explained how to make another widget pop up
         #Key is to make a widget and use self.widget
         file = file_browser("File Browser").openFileNameDialog()
         if(file != None):
 
-            self.csv_importer = csv_importer_popup("CSV Importer",'test.db',self.table_in_db)
+            self.csv_importer = csv_importer_popup("CSV Importer",'test.db',self.tables_in_db)
             #This links the import signal to the import close window
             self.csv_importer.importDoneSignal.connect(self.import_closed)
             self.csv_importer.run_popup(file)
@@ -246,6 +190,10 @@ class Ui_MainWindow(QtWidgets.QWidget):
             self.csv_importer.exec_()
 
     def import_closed(self,str):
+        """
+        When the csv importer is closed a signal is emitted with the table
+        name of the imported table. It updates various refernces to the table name
+        """
         #This method is called when the import window closed
         print('Refreshing table')
         #Call the update menu action to refresh the table
@@ -256,6 +204,9 @@ class Ui_MainWindow(QtWidgets.QWidget):
 
 
     def set_curr_table_name(self, new_table_name):
+        """
+        Sets the current table to the paramater
+        """
         self.curr_table = new_table_name
 
     def switch_curr_table_menubar(self):
@@ -271,19 +222,58 @@ class Ui_MainWindow(QtWidgets.QWidget):
         When a combo box option is clicked it changes the current
         Table to the option's text and refreshes the table
         """
-        print(self.table_in_db[selectedItem])
-        self.set_curr_table_name(self.table_in_db[selectedItem])
+        print(self.tables_in_db[selectedItem])
+        self.set_curr_table_name(self.tables_in_db[selectedItem])
         self.update_menu_action()
 
 
 
     def add_menu_items(self,table_names,menu):
+        """
+        Adds a action to the menu provided by creating new actions using the
+        table names list inputed
+        """
         for name in table_names:
             menu.addAction(name,self.switch_curr_table_menubar)
 
     def add_combo_box_items(self,table_names,comboBox):
+        """
+        Creates combo box entries for each table in the data base. Each entry
+        is clickable and when clicked will update the table
+        """
         for name in table_names:
             comboBox.addItem(name)
+
+    def clear_table(self):
+        """
+        Clears the current table and verifies if the users wants to clear it
+        """
+        choice  = QtWidgets.QMessageBox.question(self, 'Confimation',
+                                    "Are you sure you want to clear the current table?",
+                                    QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+        if choice == QtWidgets.QMessageBox.Yes:
+            self.db.clear_table(self.curr_table)
+            self.update_menu_action()
+
+    def delete_table(self):
+        """
+        Delete the current table and verifies if the users wants to delete it
+        """
+        choice  = QtWidgets.QMessageBox.question(self, 'Confimation',
+                                    "Are you sure you want to delete the current table? \nTHIS OPERATION IS NOT RECOVERABLE",
+                                    QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+        if choice == QtWidgets.QMessageBox.Yes:
+            self.db.delete_table(self.curr_table)
+            self.update_view_menu()
+            self.update_combobox()
+
+            if(len(self.tables_in_db) == 0):
+                self.update_table([],[])
+            else:
+                #Update the table to show another table
+                pass
+
+
 
 
     def update_view_menu(self):
@@ -292,8 +282,8 @@ class Ui_MainWindow(QtWidgets.QWidget):
         any new tables that were added during an import
         """
         self.viewMenu.clear()
-        self.table_in_db = self.db.get_table_names()
-        self.add_menu_items(self.table_in_db
+        self.tables_in_db = self.db.get_table_names()
+        self.add_menu_items(self.tables_in_db
                             ,self.viewMenu.addMenu("Switch Table"))
         self.viewMenu.addAction("Update Table", self.update_menu_action)
 
@@ -303,37 +293,44 @@ class Ui_MainWindow(QtWidgets.QWidget):
         any new tables that were added during an import
         """
         self.comboBox.clear()
-        self.add_combo_box_items(self.table_in_db,self.comboBox)
+        self.add_combo_box_items(self.tables_in_db,self.comboBox)
 
 
     def table_item_clicked(self,row,column):
+        """
+        When a cell is clicked inside the table this retrieves its row
+        from the data base for the pop up window to display
+        """
         #The double clicked signal returns the row and column of the
         #double clicked item. It will automatically pass those into the method
         #if the paramaters are row and column
         selectedRow = self.db.get_row_at(table_name = self.curr_table, row_id = row + 1)
         print(selectedRow)
         #Here you would call a method to show the profile page
-<<<<<<< HEAD
-=======
-    
+
     def get_search_key(self):
+        """
+        Gets the text from the search bar and checks if it is a valid entry. If so
+        then pass it to the search table method
+        """
         print("CLICKED")
-        key = self.searchBar.displayText()
-        print(key)
-        self.search_table(key)
-         
+        try:
+            key = self.searchBar.displayText()
+            if(self.db.is_valid_string(key)):
+                print(key)
+                self.search_table(key)
+        except:
+            QtWidgets.QMessageBox.critical(self, 'Invalid Text',
+                "Search values can only have alphanumeric values",
+                QtWidgets.QMessageBox.Ok)
+
     def search_table(self, search_key):
+        """
+        Searches the database for the search key inputed
+        """
         rows = self.db.search_table(search_key, self.curr_table)
         print(rows)
         return rows
-    
-    def run(self,width,height):
-        app = QtWidgets.QApplication(sys.argv)
-        self.mainWindow = QMainWindow()
-        self.setupUi(self.mainWindow,width,height)
-        self.mainWindow.show()
-        sys.exit(app.exec_())
->>>>>>> 8914bf8502fc8df5cfe98677ed0ce0fc0ba2a092
 
 if __name__ == '__main__':
     data_base_file = 'test.db'
@@ -342,7 +339,6 @@ if __name__ == '__main__':
     mainWindow.resize(1600 , 900+50)
     leadsTable = Ui_MainWindow(data_base_file)
     leadsTable.setup_main_widget(1600,900)
-
     mainWindow.setCentralWidget(leadsTable)
 
     menuBar = leadsTable.setup_menu_bar()
