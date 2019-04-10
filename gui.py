395fc8f75c9508c7f5532f856cf5cbfe619f2edc
ulycar11 <46250723+ulycar11@users.noreply.github.com>
2019-04-10 04:17:38 -0500

    Fixed small spacing issues

--- gui.py
+++ gui.py
@@ -1,17 +1,16 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 from ShowList import Ui_MainWindow
+from PyQt5.Qt import QDesktopWidget
 
 class gui(QtWidgets.QMainWindow):
     def __init__(self,width,height,db_file_loc):
         super().__init__()
         self.setObjectName("Direct Marketing CMR")
         self.setWindowTitle("Direct Marketing CMR")
-        self.resize(width , height+50)
-
+        self.resize(width , height + 50)
         self.main_window = Ui_MainWindow(db_file_loc)
         self.main_window.setup_main_widget(width,height)
-        print(type(self.main_window))
         self.setCentralWidget(self.main_window)
 
         self.menuBar = self.main_window.setup_menu_bar()
